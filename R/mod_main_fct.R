bae00dcb97cc28a4535aeb9d84bc5fcb84499ea5
FrankLef <flefebvre01@hotmail.com>
2022-02-23 17:24:43 -0500

    test

--- R/mod_main_fct.R
+++ R/mod_main_fct.R
@@ -7,7 +7,7 @@
 #' @importFrom stats rbinom
 #' @importFrom rlang .data
 #'
-#' @return Dataframe, summary of binomial distribution probabilities 
+#' @return Dataframe, summary of binomial istribution probabilities 
 #'
 #' @examples
 #' sim_binom()
